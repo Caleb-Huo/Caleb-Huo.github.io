f626a5f96b75aa485467cac99be55486233ec830
Caleb-Huo <zhuo@ufl.edu>
2017-09-27 15:03:53 -0400

    update autoGrading

--- teaching/2017FALL/HW/HW1/solution/grade.R
+++ teaching/2017FALL/HW/HW1/solution/grade.R
@@ -1,3 +1,8 @@
+setwd("~/Dropbox/teaching/2017FALL/HW/HW1/grading")
+
+standard <- get(load("hw1_Huo_Zhiguang.Rdata"))
+
+
 grading <- function(standard, submission, tol=1e-2){
 	studentName <- paste(submission$firstName, submission$lastName)
 	fixedTitle <- c("firstName", "lastName", "GatorLinkID")
@@ -23,25 +28,23 @@ grading <- function(standard, submission, tol=1e-2){
 	 
 	
 	B4_a <- tryCatch(submission$B4("a"), error=function(e) {-1}) == -1
-	B4_b <- tryCatch(submission$B4(x=1:4,a=1:3), error=function(e) {-1}) == -1
 	B4_c <- tryCatch(submission$B4(x=1:4, a=-3), error=function(e) {-1}) == -1
 	B4_d <- all(tryCatch(submission$B4(x=1:4, a=2), error=function(e) {-1}) == tryCatch(standard$B4(x=1:4, a=2), error=function(e) {-1}))
-	B4_all <- list(B4_a, B4_b, B4_c, B4_d)
+	B4_all <- list(B4_a, B4_c, B4_d)
 	if(any(sapply(B4_all,length) == 0)){
 	  scores["B4"] <- 0
 	} else {
-		scores["B4"] <- B4_a && B4_b && B4_c && B4_d
+		scores["B4"] <- B4_a  && B4_c && B4_d
 	}
 	
 	B5_a <- tryCatch(tryCatch(submission$B5("a"), error=function(e) {-2}) == standard$B5("a"), error=function(ee) 0)
-	B5_b <- tryCatch(tryCatch(submission$B5(x=1:4,a=1:3), error=function(e) {-2}) == standard$B5(x=1:4,a=1:3), error=function(ee) 0)
 	B5_c <- tryCatch(tryCatch(submission$B5(x=1:4, a=-3), error=function(e) {-2}) == standard$B5(x=1:4, a=-3), error=function(ee) 0)
 	B5_d <- tryCatch(all(tryCatch(submission$B5(x=1:4, a=2), error=function(e) {-2}) == standard$B5(x=1:4, a=2)), error=function(ee) 0)
-	B5_all <- list(B5_a, B5_b, B5_c, B5_d)
+	B5_all <- list(B5_a, B5_c, B5_d)
 	if(any(sapply(B5_all,length) == 0)){
 	  scores["B5"] <- 0
 	} else {
-	  scores["B5"] <- B5_a && B5_b && B5_c && B5_d
+	  scores["B5"] <- B5_a  && B5_c && B5_d
 	}
   
 	
@@ -51,14 +54,48 @@ grading <- function(standard, submission, tol=1e-2){
 	scores["C3a"] <- all(submission$C3a %in% standard$C3a) & all(standard$C3a %in% submission$C3a)
 	scores["C3b"] <- all(submission$C3b %in% standard$C3b) & all(standard$C3b %in% submission$C3b)
 	scores["C4"] <- all(submission$C4 %in% standard$C4) & all(standard$C4 %in% submission$C4)
-	scores["C5a"] <- abs(submission$C1a - standard$C1a) < tol * standard$C1a
-	scores["C5b"] <- abs(submission$C1b - standard$C1b) < tol * standard$C1b
+	scores["C5a"] <- abs(submission$C5a - standard$C5a) < tol * standard$C5a
+	scores["C5b"] <- abs(submission$C5b - standard$C5b) < tol * standard$C5b
 	scores["C6"] <- abs(submission$C6 - standard$C6) < tol * standard$C6
 	
 	gradeInfo <- list(name=studentName, scores=scores, grade=sum(scores * scoreTitle) + 10)
 	return(gradeInfo)
 }
 
+
+submissionFiles0 <- dir("submission", full.names = T)
+submissionFiles <- setdiff(submissionFiles0, c("submission/HW1_Hasan_Hasibul.rds",
+                                               "submission/hw1_Subedi_Pooja-1.Rdata"))
+
+names <- NULL
+finalGrades <- NULL
+details <- NULL
+
+for(i in 1:length(submissionFiles)){
+  afile <- submissionFiles[i]
+  cat("grading ",afile, "\n")
+  submission <- get(load(afile))
   
-## test
-## agrading <- grading(standard=standard, submission=submission)
+  agrading <- grading(standard=standard, submission=submission)
+  
+  names <- c(names, agrading$name)
+  finalGrades <- c(finalGrades, agrading$grade)
+  details <- cbind(details, agrading$scores)
+}
+
+colnames(details) <- names
+names(finalGrades) <- names
+
+write.csv(finalGrades, "finalGrades.csv")
+
+png("gradeDistHW1.png")
+hist(finalGrades,nclass=10, main="HW1 grade")
+dev.off()
+
+## analysize the grade
+questionCount <- rowSums(details)
+
+png("questionCountHW1.png")
+barplot(questionCount)
+dev.off()
+
