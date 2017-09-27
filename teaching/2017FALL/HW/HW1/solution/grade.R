7fb9340e3cd9dfd1532452cab819ed4b682452cd
Caleb-Huo <zhuo@ufl.edu>
2017-09-26 22:02:15 -0400

    HW1 solution

---
+++ teaching/2017FALL/HW/HW1/solution/grade.R
@@ -0,0 +1,64 @@
+grading <- function(standard, submission, tol=1e-2){
+	studentName <- paste(submission$firstName, submission$lastName)
+	fixedTitle <- c("firstName", "lastName", "GatorLinkID")
+	titles <- setdiff(names(standard), fixedTitle)
+	scoreTitle <- rep(6,length(titles))
+	scores <- numeric(length(titles))
+	names(scores) <- titles
+	names(scoreTitle) <- titles
+	endW <- sapply(titles,function(x) substring(x,nchar(x), nchar(x)))
+	scoreTitle[ endW == "a" | endW == "b"] <- 3
+	
+	scores["A1"] <- abs(submission$A1 - standard$A1) < tol * standard$A1
+	scores["A2"] <- all(submission$A2 %in% standard$A2) & all(standard$A2 %in% submission$A2)
+	scores["A3"] <- all(submission$A3 %in% standard$A3) & all(standard$A3 %in% submission$A3)
+	scores["A4"] <- all(submission$A4 %in% standard$A4) & all(standard$A4 %in% submission$A4)
+	scores["B1"] <- abs(submission$B1 - standard$B1) < tol * standard$B1
+	scores["B2"] <- abs(submission$B2 - standard$B2) < tol * standard$B2
+	if(!is.numeric(submission$B3)){
+	  scores["B3"] <- 0 
+	} else {
+	  scores["B3"] <- abs(submission$B3 - standard$B3) < tol * standard$B3
+	}
+	 
+	
+	B4_a <- tryCatch(submission$B4("a"), error=function(e) {-1}) == -1
+	B4_b <- tryCatch(submission$B4(x=1:4,a=1:3), error=function(e) {-1}) == -1
+	B4_c <- tryCatch(submission$B4(x=1:4, a=-3), error=function(e) {-1}) == -1
+	B4_d <- all(tryCatch(submission$B4(x=1:4, a=2), error=function(e) {-1}) == tryCatch(standard$B4(x=1:4, a=2), error=function(e) {-1}))
+	B4_all <- list(B4_a, B4_b, B4_c, B4_d)
+	if(any(sapply(B4_all,length) == 0)){
+	  scores["B4"] <- 0
+	} else {
+		scores["B4"] <- B4_a && B4_b && B4_c && B4_d
+	}
+	
+	B5_a <- tryCatch(tryCatch(submission$B5("a"), error=function(e) {-2}) == standard$B5("a"), error=function(ee) 0)
+	B5_b <- tryCatch(tryCatch(submission$B5(x=1:4,a=1:3), error=function(e) {-2}) == standard$B5(x=1:4,a=1:3), error=function(ee) 0)
+	B5_c <- tryCatch(tryCatch(submission$B5(x=1:4, a=-3), error=function(e) {-2}) == standard$B5(x=1:4, a=-3), error=function(ee) 0)
+	B5_d <- tryCatch(all(tryCatch(submission$B5(x=1:4, a=2), error=function(e) {-2}) == standard$B5(x=1:4, a=2)), error=function(ee) 0)
+	B5_all <- list(B5_a, B5_b, B5_c, B5_d)
+	if(any(sapply(B5_all,length) == 0)){
+	  scores["B5"] <- 0
+	} else {
+	  scores["B5"] <- B5_a && B5_b && B5_c && B5_d
+	}
+  
+	
+	scores["C1a"] <- abs(submission$C1a - standard$C1a) < tol
+	scores["C1b"] <- abs(submission$C1b - standard$C1b) < tol
+	scores["C2"] <- all(submission$C2 %in% standard$C2) & all(standard$C2 %in% submission$C2)
+	scores["C3a"] <- all(submission$C3a %in% standard$C3a) & all(standard$C3a %in% submission$C3a)
+	scores["C3b"] <- all(submission$C3b %in% standard$C3b) & all(standard$C3b %in% submission$C3b)
+	scores["C4"] <- all(submission$C4 %in% standard$C4) & all(standard$C4 %in% submission$C4)
+	scores["C5a"] <- abs(submission$C1a - standard$C1a) < tol * standard$C1a
+	scores["C5b"] <- abs(submission$C1b - standard$C1b) < tol * standard$C1b
+	scores["C6"] <- abs(submission$C6 - standard$C6) < tol * standard$C6
+	
+	gradeInfo <- list(name=studentName, scores=scores, grade=sum(scores * scoreTitle) + 10)
+	return(gradeInfo)
+}
+
+  
+## test
+## agrading <- grading(standard=standard, submission=submission)
