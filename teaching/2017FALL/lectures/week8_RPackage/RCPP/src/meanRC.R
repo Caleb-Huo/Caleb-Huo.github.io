ac29d68bfbe1774ecd92427d111c5b581e73747d
Caleb-Huo <zhuo@ufl.edu>
2017-10-17 16:47:57 -0400

    update RCPP lecture

---
+++ teaching/2017FALL/lectures/week8_RPackage/RCPP/src/meanRC.R
@@ -0,0 +1,13 @@
+##' mean calling c
+##'
+##' mean calling c balalal
+##' @title mean calling c
+##' @param x a vector
+##' @return mean of the vector
+##' @author Caleb
+##' @export
+##' @examples
+##' meanRC(1:10)
+meanRC <- function(x){
+  meanC(x)
+}
